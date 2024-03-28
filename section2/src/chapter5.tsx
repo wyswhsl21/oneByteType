7c83c0d914fc419438a64eeb67ef95ada594f7fd
DESKTOP-QR42JKC\wyswh <wyswhsl21@naver.com>
2024-03-28 23:59:45 +0900

    enum type 공부하기

---
+++ section2/src/chapter5.tsx
@@ -0,0 +1,19 @@
+//enum 타입
+//여러가지 값들에 각각 이름을 부여해 열거해두고 사용하는 타입
+enum Role {
+  ADMIn,
+  USER,
+  GUEST,
+}
+const user1 = {
+  name: "rlawodn",
+  role: 0,
+};
+const user2 = {
+  name: "jaewoo",
+  role: 1,
+};
+const user3 = {
+  name: "아무개",
+  role: 2,
+};
