7c3de9eb77ce32271c208fb853e1e32b3a3c9720
DESKTOP-QR42JKC\wyswh <wyswhsl21@naver.com>
2024-04-02 00:17:48 +0900

    2024-04-02 대수타입의 타입스크립트 공부

--- section3/src/chapter3.ts
+++ section3/src/chapter3.ts
@@ -26,5 +26,61 @@ let girlmodel: model = {
   tall: 178,
 };
 
-let girl = girlmodel;
-let girlmodel = girl;
+/**
+*@title 객체에서 프로퍼티가 많다고 슈퍼타입이 아님 . 프로퍼티가 적은 객체가 슈퍼타입이 되고 프로퍼티가 많은 객체는 
+서브타입이 됨.
+*@description
+*/
+
+/**
+ *@title 대수타입
+ *@description ->여러개의 타입을 합성해서 새롭게 만들어낸 타입
+ * -> 합집합 타입과 교집합 타입이 존재합니다.
+ */
+
+/**
+ *@title 1.합집합 - Union 타입
+ *@description
+ */
+let a: string | number | boolean | undefined;
+a = 1;
+a = "hello";
+
+a = true;
+let arr: (number | string | boolean)[] = [1, "hello", true];
+
+type Dog = {
+  name: string;
+  color: string;
+};
+type Person = {
+  name: string;
+  language: string;
+};
+
+type Union1 = Dog | Person;
+let union1: Union1 = {
+  name: "",
+  color: "",
+};
+
+let union2: Union1 = {
+  name: "",
+  language: "",
+};
+
+let union3: Union1 = {
+  name: "",
+  color: "",
+  language: "",
+};
+
+// let union4: Union1 = {
+//   name: "",
+// };
+
+/**
+ *@title 교집합 타입 - Intersection 타입
+ *@description
+ */
+let variable: number & string;
